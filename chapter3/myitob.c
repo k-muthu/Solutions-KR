2978cc069a42ba4e027999c8ebb0bcfa313c5665
muthu <kmuthukrishna@gmail.com>
2020-12-11 20:29:53 +0530

    modified myitob function

--- chapter3/myitob.c
+++ chapter3/myitob.c
@@ -1,63 +1,85 @@
-#include<stdio.h>
-#include<limits.h>
+#include <stdio.h>
+#include <limits.h>
 
-#define MAXLINE 1000
+#define MAXLINE 20
 
-/*
-This program implements myitob(int n, char s[], int b).
-Converts a given integer in base 10 to a string 
-representaion of the integer in base b.
-*/
+void myitob(int n, char s[], int b);
+void strreverse(char s[]);
+int len(char s[]);
 
-void myitob(int n, char s[], int base);
-void reverse(char s[]);
-int mystrlen(char s[]);
-int positive(int n);
+int main()
+{
+	int num = INT_MIN;
+	char s[MAXLINE];
 
-int main(){
-    int base, n;
-    char s[MAXLINE];
-    n = INT_MIN;
-    base = 16;
-    myitob(n, s, base);
-    printf("%s\n", s);
-    return 0;
+	myitob(num, s, 8);
+	printf("%s\n", s);
+	return 0;
 }
 
-int mystrlen(char s[]){
-    int len;
-    for(len = 0; s[len] != '\0'; ++len);
-    return len;
+/* myitoa : converts a number to a string */
+void myitob(int n, char s[], int b)
+{
+	int i, j, sign, min;
+
+	i = 0;
+	min = 0;
+	sign = 1;
+	if (n < 0) {
+		sign = -1;
+		if (n == INT_MIN) {
+			min = 1;
+			n = INT_MAX;
+		}
+		else
+			n = -n;
+	}
+	do {
+		s[i++] = (n % b > 9) ? (n % b + 'A' - 10) : (n % b + '0');
+	} while ((n /= b) > 0);
+
+	/* INT_MAX and INT_MIN differ by one in the units place when absolute value is taken */
+	if (min == 1) {
+		/* to find if the base is a power of 2 */
+		j = 1 << 1;
+		while(j) {
+			if ((b & j) == b)
+				break;
+			else
+				j <<= 1;
+		}
+		/* if b is a power of 2 */
+		if (j) {
+			for (j = 0; j < i - 1; ++j)
+				s[j] = '0';
+			++s[j];
+		}
+		else
+			++s[0];
+	}
+	if (sign < 0)
+		s[i++] = '-';
+	s[i] = '\0';
+	strreverse(s);
 }
 
-void reverse(char s[]){
-    int c;
-    int i, j;
-    for(i = 0, j = mystrlen(s) - 1; i < j; ++i, --j){
-        c = s[i];
-        s[i] = s[j];
-        s[j] = c;
-    }
+/* strreverse : reverse the input string */
+void strreverse(char s[])
+{
+	int i, j, temp;
+
+	for (i = 0, j = len(s) - 1; i < j; ++i, --j) {
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
+	}
 }
 
-int positive(int n){
-    return (n > 0) ? n : -n;
-}
+/* len : returns string length */
+int len(char s[])
+{
+	int i;
 
-void myitob(int n, char s[], int base){
-    int j, c, sign;
-    sign = n;
-
-    j = 0;
-    do {
-        c = positive(n % base) + '0';
-        s[j++] = (c > '9') ? (c + 'A' - '9' - 1) : c; 
-
-    }while(n /= base);
-    
-    if(sign < 0)
-        s[j++] = '-';
-
-    s[j] = '\0';
-    reverse(s);    
+	for ( i = 0; s[i] != '\0'; ++i);
+	return i;
 }
