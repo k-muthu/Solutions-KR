9fc5bb7598bbd99998f815ebbfa427bebfb44b47
muthu <kmuthukrishna@gmail.com>
2020-12-11 17:59:45 +0530

    modified myitoa function

--- chapter3/myitoa.c
+++ chapter3/myitoa.c
@@ -1,63 +1,69 @@
-#include<stdio.h>
-#include<limits.h>
+#include <stdio.h>
+#include <limits.h>
 
-#define MAXLINE 1000
+#define MAXLINE 20
 
-/*
-This program implements myitoa(int n, char s[]).
-Converts a given integer to a string
-*/
+void myitoa(int n, char s[]);
+void strreverse(char s[]);
+int len(char s[]);
 
-void myitoa(int n, char s1[]);
-int mystrlen(char s[]);
-void reverse(char s[]);
-int positive(int n);
+int main()
+{
+	int num = INT_MIN + 1;
+	char s[MAXLINE];
 
-int main(){
-    int n = ~(1 << 31) + 1;
-    char s1[MAXLINE];
-    myitoa(n, s1);
-    printf("%s\n", s1);
-    return 0;
+	myitoa(num, s);
+	printf("%s\n", s);
+	return 0;
 }
 
-int positive(int n){
-    return (n > 0) ? n : -n;
+/* myitoa : converts a number to a string */
+void myitoa(int n, char s[])
+{
+	int i, sign, min;
+
+	i = 0;
+	min = 0;
+	sign = 1;
+	if (n < 0) {
+		sign = -1;
+		if (n == INT_MIN) {
+			min = 1;
+			n = INT_MAX;
+		}
+		else
+			n = -n;
+	}
+	do {
+		s[i++] = n % 10 + '0';
+	} while ((n /= 10) > 0);
+
+	/* INT_MAX and INT_MIN differ by one in the units place when absolute value is taken */
+	if (min == 1)
+		s[0] = s[0] + 1;
+	if (sign < 0)
+		s[i++] = '-';
+	s[i] = '\0';
+	strreverse(s);
 }
 
-int mystrlen(char s[]){
-    int len = 0;
-    for(len = 0; s[len] != '\0'; ++len);
-    return len;
+/* strreverse : reverse the input string */
+void strreverse(char s[])
+{
+	int i, j, temp;
+	
+	for (i = 0, j = len(s) - 1; i < j; ++i, --j) {
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
+	}
 }
 
-void reverse(char s[]){
-    int len = mystrlen(s);
-    int forward, backward, c;
-    for(forward = 0, backward = len - 1; forward < backward; ++forward, --backward){
-        c = s[forward];
-        s[forward] = s[backward];
-        s[backward] = c;
-    }
-}
+/* len : returns string length */
+int len(char s[])
+{
+	int i;
 
-/* 
-In C, % is defined as the remainder operator
-and according to C99 standard it is defined to be
-a == ((a/b) * b) + (a % b)
-*/
-void myitoa(int n, char s1[]){
-    int j, sign;
-    sign = n;
-
-    j = 0;
-    do {
-        s1[j++] = positive(n % 10) + '0';
-    }while(n /= 10);
-
-    if(sign < 0)
-        s1[j++] = '-';
-
-    s1[j] = '\0';
-    reverse(s1);
+	for ( i = 0; s[i] != '\0'; ++i);
+	return i;
 }
